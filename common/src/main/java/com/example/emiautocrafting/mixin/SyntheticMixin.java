581ff61a60b9f76ba048fda2d357b87a35167d02
digestlotion <keithersuars@gmail.com>
2026-06-30 22:53:54 +0800

    shameless plug

---
+++ common/src/main/java/com/example/emiautocrafting/mixin/SyntheticMixin.java
@@ -0,0 +1,30 @@
+package com.example.emiautocrafting.mixin;
+
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.example.emiautocrafting.EmiAutocraftingConfig;
+
+import dev.emi.emi.EmiPort;
+import dev.emi.emi.runtime.EmiFavorite;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+
+@Mixin(value = EmiFavorite.Synthetic.class, remap = false)
+public class SyntheticMixin {
+
+    @Inject(method = "getTooltip", at = @At(value = "RETURN"), cancellable = true)
+    private static void onGetTooltip(CallbackInfoReturnable<List<TooltipComponent>> cir) {
+        cir.getReturnValue().add(
+            TooltipComponent.of(
+                EmiPort.translatable(
+                    "tooltip.emiautocrafting.autocraft_tree",
+                    EmiAutocraftingConfig.craftTree.getBindText()
+                ).asOrderedText()
+            )
+        );
+    }
+}
