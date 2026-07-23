814017f7a2fe382ea748b643eaa10af05a331b47
digestlotion <keithersuars@gmail.com>
2026-07-23 23:28:15 +0800

    actually toms compat this time(i hope)

--- common/src/main/java/com/example/emiautocrafting/EmiAutocrafting.java
+++ common/src/main/java/com/example/emiautocrafting/EmiAutocrafting.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.tom.storagemod.gui.CraftingTerminalMenu;
+import com.tom.storagemod.gui.StorageTerminalMenu;
 
 import dev.emi.emi.api.recipe.handler.EmiCraftContext;
 import dev.emi.emi.bom.BoM;
@@ -15,6 +16,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.util.Identifier;
 
 public final class EmiAutocrafting {
 
@@ -29,14 +31,21 @@ public final class EmiAutocrafting {
         if (screen == null) return;
         ScreenHandler screenHandler = screen.getScreenHandler();
         isWorking = true;
-        try {
-            for (MaterialNode node : getTreeRecipes(BoM.tree.goal)) {
-                if (node.recipe == null || node.neededBatches <= 0) continue;
-                if (EmiRecipeFiller.performFill(node.recipe, screen, EmiCraftContext.Type.FILL_BUTTON, EmiCraftContext.Destination.INVENTORY, (int) Math.ceil(node.neededBatches))) BoM.tree.recalculate();
-                if (EmiAgnos.isModLoaded("toms_storage") && screenHandler instanceof CraftingTerminalMenu) for (int i = 0; i < Math.ceil(node.neededBatches); i++, client.interactionManager.clickSlot(screenHandler.syncId, 0, 0, SlotActionType.PICKUP, client.player));
-                return;
+        for (MaterialNode node : getTreeRecipes(BoM.tree.goal)) {
+            if (node.recipe == null || node.neededBatches <= 0) continue;
+            if (!node.recipe.supportsRecipeTree() || !node.recipe.getCategory().getId().equals(Identifier.of("minecraft", "crafting"))) continue;
+            if (EmiRecipeFiller.performFill(node.recipe, screen, EmiCraftContext.Type.FILL_BUTTON, EmiCraftContext.Destination.INVENTORY, (int) Math.ceil(node.neededBatches))) {
+                BoM.tree.recalculate();
+                if (EmiAgnos.isModLoaded("toms_storage") && screenHandler instanceof CraftingTerminalMenu menu) {
+                    for (int i = 0; i < Math.ceil(node.neededBatches); ++i) {
+                        client.interactionManager.clickSlot(menu.syncId, 0, 0, SlotActionType.PICKUP, client.player);
+                        menu.sync.sendInteract(null, StorageTerminalMenu.SlotAction.PULL_OR_PUSH_STACK, false);
+                    }
+                }
+                break;
             }
-        } finally {isWorking = false;}
+        }
+        isWorking = false;
     }
 
     public static List<MaterialNode> getTreeRecipes(MaterialNode root) {
